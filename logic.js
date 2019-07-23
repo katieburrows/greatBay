9e5b32f8453158903626fd3fbac27aee104125b9
katieburrows <katie_burrows@me.com>
2019-07-23 15:57:37 -0600

    added logic file and pseudocode

---
+++ logic.js
@@ -0,0 +1,35 @@
+//an inquirer list loads:
+    //[POST]
+    //[BID]
+    //[EXIT]
+
+
+
+    //[POST]
+        //inquirer prompt:
+            //NAME
+            //CATEGORY
+            //STARTING PRICE
+        //this info is added to the database
+    //return to main inquirer prompt
+    
+
+
+    //[BID]
+        //inquirer list of category to bid on
+        //inquirer list of items within the chosen category they can bid on
+        //once an item is selected to bid on:
+            //inquirer prompt: "how much would you like to bid?"
+                //compare bid to the starting price/previous high bid
+                    //if their bid is higher
+                        //inform user of success
+                        //replace old bid with this bid amount
+                    //if their bid is lower or equal
+                        //inform user of loss
+                        //return to main inquirer prompt
+
+
+
+//users are able to:
+    //create an item, task, job, or project
+    //bid on it (and others already added)
